c0807359e18b609db38c270949da0fbdcc30a5df
pvzzombs <pvzzombs@gmail.com>
2024-06-29 14:53:37 +0800

    Changes:
    - Try to add image minifier, not possible to import since it requires to be an ES module
    - Ignore this repository package json file from being move into the other repository
    - Prevent raw and binary files from being change

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const gulp = require("gulp");
 const minifyHTML = require('gulp-htmlmin');
 const minifyJS = require('gulp-uglify');
 const minifyCSS = require("gulp-clean-css")
+// const minifyImages = require("gulp-imagemin");
+// import imagemin from 'gulp-imagemin';
 
 const pathToDestination = "../pvzzombs_github_io/";
-const ignorePaths = ["!node_modules/**", "!gulpfile.js"];
+const ignorePaths = ["!./node_modules/**", "!./gulpfile.js", "!./package.json"];
 
 function buildHTML() {
   return gulp.src(["**/*.html", ...ignorePaths])
@@ -45,12 +47,22 @@ function buildJSON() {
 }
 
 function buildImages() {
-  return gulp.src(["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", ...ignorePaths])
+  return gulp.src(["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", ...ignorePaths],
+    { encoding: false }
+  )
   .pipe(gulp.dest(pathToDestination));
 }
 
+// function minImages() {
+//   return gulp.src(["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", ...ignorePaths])
+//   .pipe(imagemin())
+//   .pipe(gulp.dest(pathToDestination));
+// }
+
 function buildFonts() {
-  return gulp.src(["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf", ...ignorePaths])
+  return gulp.src(["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf", ...ignorePaths],
+    { encoding: false }
+  )
   .pipe(gulp.dest(pathToDestination));
 }
 
@@ -62,6 +74,7 @@ gulp.task("build-js", buildJS);
 gulp.task("minify-js", minJS);
 gulp.task("build-json", buildJSON);
 gulp.task("build-images", buildImages);
+// gulp.task("minify-images", minImages);
 gulp.task("build-fonts", buildFonts);
 
 gulp.task("other", gulp.series(buildJSON, buildImages, buildFonts));
