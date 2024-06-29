5199b2ed895ad5d1b6ff433019b50ba8409e6bb6
pvzzombs <pvzzombs@gmail.com>
2024-06-29 15:21:42 +0800

    Add logger and details log

--- gulpfile.js
+++ gulpfile.js
@@ -4,45 +4,54 @@ const minifyJS = require('gulp-uglify');
 const minifyCSS = require("gulp-clean-css")
 // const minifyImages = require("gulp-imagemin");
 // import imagemin from 'gulp-imagemin';
+const logger = require('gulp-logger');
+const log = require("fancy-log");
 
 const pathToDestination = "../pvzzombs_github_io/";
 const ignorePaths = ["!./node_modules/**", "!./gulpfile.js", "!./package.json"];
 
 function buildHTML() {
   return gulp.src(["**/*.html", ...ignorePaths])
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
 }
 
 function minHTML() {
   return gulp.src(["**/*.html", ...ignorePaths])
+  .pipe(logger())
   .pipe(minifyHTML({ collapseWhitespace: true, removeComments: true }))
   .pipe(gulp.dest(pathToDestination));
 }
 
 function buildCSS() {
   return gulp.src(["**/*.css", ...ignorePaths])
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
 }
 
 function minCSS() {
   return gulp.src(["**/*.css", ...ignorePaths])
+  .pipe(logger())
   .pipe(minifyCSS())
   .pipe(gulp.dest(pathToDestination));
 }
 
 function buildJS() {
   return gulp.src(["**/*.js", ...ignorePaths])
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
 }
 
 function minJS() {
   return gulp.src(["**/*.js", ...ignorePaths])
+  .pipe(logger())
   .pipe(minifyJS())
   .pipe(gulp.dest(pathToDestination));
 }
 
 function buildJSON() {
   return gulp.src(["**/*.json", ...ignorePaths])
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
 }
 
@@ -50,6 +59,7 @@ function buildImages() {
   return gulp.src(["**/*.png", "**/*.jpg", "**/*.jpeg", "**/*.gif", ...ignorePaths],
     { encoding: false }
   )
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
 }
 
@@ -63,7 +73,18 @@ function buildFonts() {
   return gulp.src(["**/*.woff2", "**/*.woff", "**/*.ttf", "**/*.otf", ...ignorePaths],
     { encoding: false }
   )
+  .pipe(logger())
   .pipe(gulp.dest(pathToDestination));
+}
+
+function startMessage(done) {
+  log.warn("Building distribution on the path: " + pathToDestination);
+  done();
+}
+
+function endMessage(done) {
+  log.warn("Building success!");
+  done();
 }
 
 gulp.task("build-html", buildHTML);
@@ -79,4 +100,5 @@ gulp.task("build-fonts", buildFonts);
 
 gulp.task("other", gulp.series(buildJSON, buildImages, buildFonts));
 
-gulp.task("default", gulp.series(buildHTML, minCSS, minJS, buildJSON, buildImages, buildFonts));
+gulp.task("default", gulp.series(startMessage, buildHTML, minCSS, minJS, buildJSON,
+  buildImages, buildFonts, endMessage));
