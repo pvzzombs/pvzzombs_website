991d28caa603534a89953629c7f9377c2e91c366
pvzzombs <pvzzombs@gmail.com>
2024-07-15 14:03:00 +0800

    Add credits

---
+++ pages/flappyBird/globals.js
@@ -0,0 +1,50 @@
+//the world
+var playerSprite;
+var pipesSprites;
+var backgroundSprite;
+
+// constants
+var gravity = 0.3;
+var flap = -6;
+// var friction = 0.99;
+var speed = 2;
+
+// var bird;
+// var pipes;
+// for relocating:
+var birdPos = {
+  x: 0,
+  y: 0
+};
+
+var pipesPos = [
+  {x: 0, y: 0, height: 0},
+  {x: 0, y: 0, height: 0},
+  {x: 0, y: 0, height: 0}
+];
+
+var score = 0;
+var highScore = 0;
+var speedUp = 0;
+
+// for fps
+var lastTime;
+var nowTime;
+var dt;
+var fpm;
+
+// sounds
+var sfx_die;
+var sfx_hit;
+var sfx_point;
+var sfx_wing;
+
+// fonts
+var mainFont;
+
+// bird death position
+var birdDeathX;
+var birdDeathY;
+
+// Scene Manager
+var mgr;
--- pages/flappyBird/main.js
+++ pages/flappyBird/main.js
@@ -1,54 +1,3 @@
-//the world
-var playerSprite;
-var pipesSprites;
-var backgroundSprite;
-
-// constants
-var gravity = 0.3;
-var flap = -6;
-// var friction = 0.99;
-var speed = 2;
-
-// var bird;
-// var pipes;
-// for relocating:
-var birdPos = {
-  x: 0,
-  y: 0
-};
-
-var pipesPos = [
-  {x: 0, y: 0, height: 0},
-  {x: 0, y: 0, height: 0},
-  {x: 0, y: 0, height: 0}
-];
-
-var score = 0;
-var highScore = 0;
-var speedUp = 0;
-
-// for fps
-var lastTime;
-var nowTime;
-var dt;
-var fpm;
-
-// sounds
-var sfx_die;
-var sfx_hit;
-var sfx_point;
-var sfx_wing;
-
-// fonts
-var mainFont;
-
-// bird death position
-var birdDeathX;
-var birdDeathY;
-
-// Scene Manager
-var mgr;
-
 function loadingSucess() {
 
 }
@@ -100,14 +49,6 @@ function setup() {
 
 function draw() {
   mgr.draw();
-}
-
-function death() {
-  
-}
-
-function mainGame() {
-  
 }
 
 function keyTyped() {
